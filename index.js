5dce24b33e8e04075d8d6c71a7b5eed8e26f4b26
Reza Aghajani <rezaaghajani@protonmail.com>
2021-01-20 16:06:59 +0330

    Configure env file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+require('dotenv')
 
-client.login('BOT TOKEN')
+client.login(process.env.TOKEN)
 
 client.on('ready',readyDiscord)
 
@@ -16,6 +17,5 @@ function gotMessage(msg) {
         let replies = ['Hello Sir!', ' Hey Mard' , 'Wassup Bro', 'What can i do for you?','Gol be khodi Gol nie?!!']
         var index = Math.floor(Math.random() * replies.length)
         msg.reply(replies[index])
-        
     }
 }
