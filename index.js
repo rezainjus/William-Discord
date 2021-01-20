4e2fdd18051bce4d089c9d77a52575d26cfe39bc
Reza Aghajani <rezaaghajani@protonmail.com>
2021-01-20 15:58:36 +0330

    Create Bot and Add Message Replier on Hello Word

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.login('BOT TOKEN')
+
+client.on('ready',readyDiscord)
+
+function readyDiscord() {
+    console.log("ready!")
+}
+
+client.on('message',gotMessage)
+
+function gotMessage(msg) {
+    if (msg.content === 'hello') {
+        let replies = ['Hello Sir!', ' Hey Mard' , 'Wassup Bro', 'What can i do for you?','Gol be khodi Gol nie?!!']
+        var index = Math.floor(Math.random() * replies.length)
+        msg.reply(replies[index])
+        
+    }
+}
