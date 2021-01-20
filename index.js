190c03d8fd4e3aaf6b6c91ec2ebbbcc41bc4aef0
Reza Aghajani <rezaaghajani@protonmail.com>
2021-01-20 16:29:17 +0330

    Add option to search and send gifs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-require('dotenv')
+const fetch = require('node-fetch')
+require('dotenv').config()
 
-client.login(process.env.TOKEN)
+client.login(process.env.BOTTOKEN)
 
 client.on('ready',readyDiscord)
 
@@ -12,10 +13,24 @@ function readyDiscord() {
 
 client.on('message',gotMessage)
 
-function gotMessage(msg) {
-    if (msg.content === 'hello') {
+async function gotMessage(msg) {
+    let tokens = msg.content.split(' ')
+    
+    if (tokens[0] === 'hello') {
         let replies = ['Hello Sir!', ' Hey Mard' , 'Wassup Bro', 'What can i do for you?','Gol be khodi Gol nie?!!']
-        var index = Math.floor(Math.random() * replies.length)
+        let index = Math.floor(Math.random() * replies.length)
         msg.reply(replies[index])
+    }
+    else if (tokens[0] == '*gif') {
+        let keywords = 'peaky blinders'
+        if (tokens.length > 1) {
+            keywords = tokens.slice(1,tokens.length).join(" ")
+        }
+        let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}&contentfiler=none`
+        let response = await fetch(url)
+        let json = await response.json()
+        let index = Math.floor(Math.random() * json.results.length)
+        msg.channel.send(json.results[index].url)
+        msg.channel.send("GIF from Tenor: " + keywords)
     }
 }
